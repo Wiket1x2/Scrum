ebc34cac77c5c4c31944de505302ac6bcd952085
Rafał <rafaline@vp.pl>
2019-05-21 07:39:33 +0200

    SpriteComponent.h
    Podstawowa funkcjonalność

--- GameEngine/GameEngine/src/ECS/SpriteComponent.h
+++ GameEngine/GameEngine/src/ECS/SpriteComponent.h
@@ -1,1 +1,62 @@
 #pragma once
+
+#include "Components.h"
+#include "SDL.h"
+#include "../TextureMenager.h"
+#include "Animation.h"
+#include <map>
+
+class SpriteComponent : public Component
+{
+private:
+	TransformComponent *transform;
+	SDL_Texture *texture;
+	SDL_Rect srcRect, destRect;
+
+	bool animated = false;
+	int frames = 0;
+	int speed = 100;
+	
+public:
+
+	void setTex(const char *path)
+	{
+		texture = TextureMenager::LoadTexture(path);
+	}
+
+	void init() override
+	{
+		transform = &entity->getComponent<TransformComponent>();
+
+		srcRect.x = srcRect.y = 0;
+		srcRect.h = transform->height;
+		srcRect.w = transform->width;
+	}
+
+	void update() override
+	{
+		if (animated)
+		{
+			srcRect.x = srcRect.w*static_cast<int>(SDL_GetTicks() / speed % frames);
+		}
+
+		srcRect.y = animIndex * transform->height;
+
+		destRect.x = static_cast<int>(transform->position.x) - Game::camera.x;
+		destRect.y = static_cast<int>(transform->position.y) - Game::camera.y;
+		destRect.h = transform->height*transform->scale;
+		destRect.w = transform->width*transform->scale;
+	}
+
+	void draw() override
+	{
+		TextureMenager::Draw(texture, srcRect, destRect, spriteFlip);
+	}
+
+	void Play(const char* animName)
+	{
+		frames = animations[animName].frame;
+		animIndex = animations[animName].index;
+		speed = animations[animName].speed;
+	}
+};
