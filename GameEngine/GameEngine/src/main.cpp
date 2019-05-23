465582195796920f9571b1ae256e4f648b71d4ff
Krzysztof K <krzykasz.inpg@gmail.com>
2019-05-23 13:17:30 +0200

    Main.cpp-first step completed
    Zakoczońo pracę nad plikiem main.cpp - inicjalizacja bibliotek wywołanie game i manager FPS

--- GameEngine/GameEngine/src/main.cpp
+++ GameEngine/GameEngine/src/main.cpp
@@ -14,6 +14,22 @@ int main(int argc, char *argv[])
 
 	game->init("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 640, false);
 
+	while (game->running())
+	{
+		frameStart = SDL_GetTicks();
+
+		game->handleEvents();
+		game->update();
+		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
+	}
+
 	game->clean();
 
 	return 0;
