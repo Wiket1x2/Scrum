6dd940898ec28efc9a38d52ba9ae7482d52019ff
Krzysztof K <krzykasz.inpg@gmail.com>
2019-05-27 20:26:53 +0200

    GameObject.cpp - "sketch" of implementation
    Wstepny zarys - wymaga implementacji (konsultacje z dev. odpowiedzialnym za plik GameObject.h)

---
+++ GameEngine/GameEngine/src/GameObject.cpp
@@ -0,0 +1,12 @@
+#include "GameObject.h"
+#include "TextureMenager.h"
+
+GameObject::GameObject(const char* textureSheet, int x, int y)
+{}
+
+void GameObject::Update()
+{}
+
+void GameObject::Render()
+{}
+//wymagana implementacja... (t.b.c)
