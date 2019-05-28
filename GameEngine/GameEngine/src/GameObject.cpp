3e8bc18be9070d0bdbdf898141a3ff4789ced5c7
Krzysztof K <krzykasz.inpg@gmail.com>
2019-05-28 21:48:15 +0200

    GameObject.cpp-implementation part 2
    Dokończenie implementacji GameObject.cpp

--- GameEngine/GameEngine/src/GameObject.cpp
+++ GameEngine/GameEngine/src/GameObject.cpp
@@ -2,11 +2,30 @@
 #include "TextureMenager.h"
 
 GameObject::GameObject(const char* textureSheet, int x, int y)
-{}
+{
+	objTexture = TextureMenager::LoadTexture(textureSheet);
+
+	xpos = x;
+	ypos = y;
+}
 
 void GameObject::Update()
-{}
+{
+	xpos++;
+	ypos++;
+
+	srcRect.h = 64;
+	srcRect.w = 64;
+	srcRect.x = 0;
+	srcRect.y = 0;
+
+	destRect.x = xpos;
+	destRect.y = ypos;
+	destRect.h = srcRect.h * 2;
+	destRect.w = srcRect.w * 2;
+}
 
 void GameObject::Render()
-{}
-//wymagana implementacja... (t.b.c)
+{
+	SDL_RenderCopy(Game::renderer, objTexture, &srcRect, &destRect);
+}
