6eaa44403eb1381cf39c62c8ebc4f228c8e0aed9
Mateusz <inpg.bahyrycz@gmail.com>
2019-05-30 21:14:23 +0200

    game.h

--- GameEngine/GameEngine/src/game.h
+++ GameEngine/GameEngine/src/game.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "SDL.h"
+#include "SDL_image.h"
+#include <iostream>
+#include <vector>
+
+class ColliderComponent;
+
+class Game
+{
+public:
+	int cnt;
+	Game();
+	~Game();
+
+	void init(const char * title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+
+	bool running() { return isRunning; };
+
+	static void AddTile(int srcX, int srcY, int xpos, int ypos);
+	static SDL_Renderer *renderer;
+	static SDL_Event event;
+	static std::vector<ColliderComponent*> colliders;
+	static bool isRunning;
+	static SDL_Rect camera;
+
+private:
+	SDL_Window *window;
+
+};
