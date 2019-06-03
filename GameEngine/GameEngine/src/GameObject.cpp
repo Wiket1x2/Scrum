9393f61adf8ee516f339cdcdc453a092c32c54a3
Krzysztof K <krzykasz.inpg@gmail.com>
2019-06-03 20:06:50 +0200

    'Cleaning code'
    Porządkowanie kodu + analiza poprawnosci integracji pliku GameObject.cpp, skomentowanie funkcjonalnosci

--- GameEngine/GameEngine/src/GameObject.cpp
+++ GameEngine/GameEngine/src/GameObject.cpp
@@ -3,9 +3,9 @@
 
 GameObject::GameObject(const char* textureSheet, int x, int y)
 {
-	objTexture = TextureMenager::LoadTexture(textureSheet);
+	objTexture = TextureMenager::LoadTexture(textureSheet); //zmienna odpowiedzialna za wczytanie tekstur
 
-	xpos = x;
+	xpos = x; 
 	ypos = y;
 }
 
@@ -14,18 +14,18 @@ void GameObject::Update()
 	xpos++;
 	ypos++;
 
-	srcRect.h = 64;
+	srcRect.h = 64; //ustalenie wsplorzednych rectangle 
 	srcRect.w = 64;
 	srcRect.x = 0;
 	srcRect.y = 0;
 
-	destRect.x = xpos;
+	destRect.x = xpos; 
 	destRect.y = ypos;
 	destRect.h = srcRect.h * 2;
 	destRect.w = srcRect.w * 2;
 }
 
-void GameObject::Render()
+void GameObject::Render() 
 {
-	SDL_RenderCopy(Game::renderer, objTexture, &srcRect, &destRect);
+	SDL_RenderCopy(Game::renderer, objTexture, &srcRect, &destRect); //renderowanie tekstur
 }
